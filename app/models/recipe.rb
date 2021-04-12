1b110749491509580cfeb49806861dd4e8deb674
Lizzie <lchan217@gmail.com>
2021-04-12 17:22:00 -0600

    add total_time method

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,10 @@
 class Recipe < ActiveRecord::Base
-  has_many :ingredients
-  has_many :steps
+    has_many :ingredients
+    has_many :steps
+
+    def total_time
+        if prep_time && cook_time
+            prep_time + cook_time
+        end
+    end
 end
