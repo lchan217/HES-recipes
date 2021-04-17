22c1475c23ba62cae119ccef273378fe372007ec
Lizzie <lchan217@gmail.com>
2021-04-17 16:52:34 -0600

    prevent user from deleting if 1 row left using jquery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,3 +2,50 @@
 //= require jquery_ujs 
 
 //= require cocoon
+
+$(document).on("DOMContentLoaded", hideOnLoad)
+$(document).on("click", ".add-ingredients", countIngredientsOnAdd)
+$(document).on("click", ".add-steps", countStepsOnAdd)
+$(document).on("click", ".remove-ingredients", countIngredientsOnRemove)
+$(document).on("click", ".remove-steps", countStepsOnRemove)
+
+function hideOnLoad() {
+    $('.remove-ingredients').hide()
+    $('.remove-steps').hide()
+}
+
+function countIngredientsOnAdd(){
+    const ingredientLength = $('.remove-ingredients').length
+    if(ingredientLength === 1){
+        $('.remove-ingredients').hide()
+    } else {
+        $('.remove-ingredients').show()
+    }
+}
+
+function countIngredientsOnRemove(){
+    const ingredientLength = $('.remove-ingredients').length
+    if(ingredientLength === 2){
+        $('.remove-ingredients').hide()
+    } else {
+        $('.remove-ingredients').show()
+    }
+}
+
+function countStepsOnAdd(){
+    const ingredientLength = $('.remove-steps').length
+    if(ingredientLength === 1){
+        $('.remove-steps').hide()
+    } else {
+        $('.remove-steps').show()
+    }
+}
+
+function countStepsOnRemove(){
+    const ingredientLength = $('.remove-steps').length
+    if(ingredientLength === 2){
+        $('.remove-steps').hide()
+    } else {
+        $('.remove-steps').show()
+    }
+}
