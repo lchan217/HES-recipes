5d34e18ff3c3f78e90fd815ad8e9d13f1cde2de6
Lizzie <lchan217@gmail.com>
2021-04-18 18:57:42 -0600

    fix jquery for editing

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,16 +3,16 @@
 
 //= require cocoon
 
-$(document).on("DOMContentLoaded", hideOnLoad)
+$(document).on("DOMContentLoaded", countIngredientsOnAdd)
 $(document).on("click", ".add-ingredients", countIngredientsOnAdd)
 $(document).on("click", ".add-steps", countStepsOnAdd)
 $(document).on("click", ".remove-ingredients", countIngredientsOnRemove)
 $(document).on("click", ".remove-steps", countStepsOnRemove)
 
-function hideOnLoad() {
-    $('.remove-ingredients').hide()
-    $('.remove-steps').hide()
-}
+// function hideOnLoad() {
+//     $('.remove-ingredients').hide()
+//     $('.remove-steps').hide()
+// }
 
 function countIngredientsOnAdd(){
     const ingredientLength = $('.remove-ingredients').length
@@ -25,6 +25,7 @@ function countIngredientsOnAdd(){
 
 function countIngredientsOnRemove(){
     const ingredientLength = $('.remove-ingredients').length
+    $(this).remove()
     if(ingredientLength === 2){
         $('.remove-ingredients').hide()
     } else {
@@ -43,6 +44,7 @@ function countStepsOnAdd(){
 
 function countStepsOnRemove(){
     const ingredientLength = $('.remove-steps').length
+    $(this).remove()
     if(ingredientLength === 2){
         $('.remove-steps').hide()
     } else {
